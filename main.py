8455059eaf798c5f1cb8cea3498fbfc99c47840d
aorlov05 <aorlov@udel.edu>
2023-11-08 22:32:28 -0500

    Created a cannon which represents the player

--- main.py
+++ main.py
@@ -2,20 +2,49 @@ from dataclasses import dataclass
 from designer import *
 
 
+# Constants which represent the ground height and position
+HEIGHT_OF_GROUND = 100
+TOP_OF_GROUND_Y = get_height() - HEIGHT_OF_GROUND
+
+
+@dataclass
+class Player:
+    cannon: DesignerObject
+    wheel: DesignerObject
+
+
 @dataclass
 class World:
     ground: DesignerObject
+    player: Player
 
 
 def create_world() -> World:
     """
-    Create a new world with green grass ground
+    Creates a new world with the initial state of ground and the player
 
     Returns:
         A new designer world instance
     """
-    ground = Rectangle('green', get_width(), 100, 0, get_height() - 100, anchor='topleft')
-    return World(ground)
+    ground = Rectangle('green', get_width(), HEIGHT_OF_GROUND, 0,
+                       TOP_OF_GROUND_Y, anchor='topleft')
+    player = create_player()
+    return World(ground, player)
+
+
+def create_player() -> Player:
+    """
+    Creates a new player object which consists of a cannon and a wheel
+    and sets it on top of the ground.
+
+    Returns:
+        A player object representing the user
+    """
+    cannon = image("./cannon.png", anchor="midtop")
+    wheel = image("./wheel.png", anchor="midbottom")
+    wheel.y = TOP_OF_GROUND_Y
+    cannon.y = wheel.y - cannon.height
+    return Player(cannon, wheel)
 
 
 when('starting', create_world)
